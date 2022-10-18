4a8297a073a142e299326ef1ddc061d99b1e9f56
Hossein Hazratgholizadeh <hossein.hazratgholizade@gmail.com>
2022-10-18 07:06:58 +0300

    Update validator

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -8,6 +8,9 @@ const CourseInput = props => {
   const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0){
+      setIsValid(true)
+    }
     setEnteredValue(event.target.value);
   };
 
@@ -19,13 +22,19 @@ const CourseInput = props => {
     }
     setIsValid(true)
     props.onAddGoal(enteredValue);
+    setEnteredValue('')
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+            style={{background: !isValid ? 'salmon' : 'transparent', borderColor:!isValid ? 'red' : '#ccc'}}
+            type="text"
+            onChange={goalInputChangeHandler}
+            value={enteredValue}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
